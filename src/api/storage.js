46257aac901c8a240c700911be0a8aa539efc926
JaviGF5 <67098919+JaviGF5@users.noreply.github.com>
2023-10-11 21:37:04 +0200

    Formulario comentado.js

--- src/api/storage.js
+++ src/api/storage.js
@@ -1,29 +1,29 @@
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+// import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 
-export class Storage {
+// export class Storage {
 
-    // Upload to Firebse
-    async uploadFile(file, folder, nameFile) {
-        try {
-            const storage  = getStorage();
-            // Place to Save
-            const fileRef = ref(storage, `${folder}/${nameFile}`);
-            return await uploadBytes(fileRef, file);
-        } catch (error) {
-            throw error;
-        }
-    }
+//     // Upload to Firebse
+//     async uploadFile(file, folder, nameFile) {
+//         try {
+//             const storage  = getStorage();
+//             // Place to Save
+//             const fileRef = ref(storage, `${folder}/${nameFile}`);
+//             return await uploadBytes(fileRef, file);
+//         } catch (error) {
+//             throw error;
+//         }
+//     }
 
-    // Update files in APP
-    async getUrlFile(pathFile) {
-        try {
-            const storage = getStorage();
-            // Get Storage Place
-            const fileRef = ref(storage, pathFile);
-            return await getDownloadURL(fileRef);
-        } catch (error) {
-            throw error;
-        }
-    }
-}
+//     // Update files in APP
+//     async getUrlFile(pathFile) {
+//         try {
+//             const storage = getStorage();
+//             // Get Storage Place
+//             const fileRef = ref(storage, pathFile);
+//             return await getDownloadURL(fileRef);
+//         } catch (error) {
+//             throw error;
+//         }
+//     }
+// }
